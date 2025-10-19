8f0b28140d66e660841e5731d982cf375cb216c4
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-10-19 23:05:28 +0600

    added the github sign In

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,24 +2,28 @@ import React, { useState } from 'react';
 import { LuLogIn } from "react-icons/lu";
 import { BiLogoGmail } from "react-icons/bi";
 import { FaGithub } from "react-icons/fa";
-import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import { auth } from '../../Firebase/firebase.config';
 import NoUser from '../NoUser/NoUser';
 import UserFound from './UserFound/UserFound';
 import { FaSignOutAlt } from "react-icons/fa";
 import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const googleAuthProvider = new GoogleAuthProvider();
+const githubAuthProvider = new GithubAuthProvider();
 
 const Login = () => {
 
     const [user, setUser] = useState(null);
+    const [provider, setProvider] = useState('');
 
     const handleGoogleSignInButtonClick = () => {
-        console.log('The Button is Clicked!');
+        // console.log('The Button is Clicked!');
         signInWithPopup(auth, googleAuthProvider)
             .then((result) => {
-                console.log(result.user);
+                setProvider('google')
+                // console.log(result.user);
                 setUser(result.user);
                 toast('Successfully Signed In!');
             })
@@ -33,12 +37,34 @@ const Login = () => {
             .then(() => {
                 toast("SuccessFully Signed Out!");
                 setUser(null);
+                setProvider('');
             })
+            .catch((error => {
+                console.log(error);
+            }))
 
     }
 
-    const handleGithubButtonOnClick = () => {
-        console.log('Github Button is Clicked!');
+    const handleGithubButtonSignOnClick = () => {
+        signInWithPopup(auth, githubAuthProvider)
+            .then((result) => {
+                setProvider('github');
+                setUser(result.user)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+
+    const handleGithubButtonSignOutClick = () => {
+        signOut(auth)
+            .then(() => {
+                setProvider('');
+                setUser(null);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
     }
     return (
         <div className='p-10 flex flex-col items-center justify-center rounded-2xl space-y-4 bg-base-200 shadow-md'>
@@ -51,7 +77,7 @@ const Login = () => {
             </div>
             <div>
                 {
-                    user ? (
+                    (user && provider === 'google') ? (
                         <button className='flex items-center
                 gap-2 bg-red-500 hover:bg-red-600 text-white font-medium px-5 py-2 rounded-lg shadow transition-all duration-300'
                             onClick={handleGoogleSignOut}>
@@ -67,11 +93,17 @@ const Login = () => {
                 }
             </div>
             <div>
-                <button className='flex items-center gap-2 font-medium bg-gray-800 hover:bg-gray-900 text-white px-5 py-2 rounded-lg shadow transition-all duration-300'
-                    onClick={handleGithubButtonOnClick}>
-                    <FaGithub />
-                    Sign Up With Github
-                </button>
+                {
+                    (user && provider === 'github') ? (<button className='flex items-center gap-2 font-medium bg-gray-800 hover:bg-gray-900 text-white px-5 py-2 rounded-lg shadow transition-all duration-300'
+                        onClick={handleGithubButtonSignOutClick}>
+                        <FaSignOutAlt />
+                        Sign Out
+                    </button>) : (<button className='flex items-center gap-2 font-medium bg-gray-800 hover:bg-gray-900 text-white px-5 py-2 rounded-lg shadow transition-all duration-300'
+                        onClick={handleGithubButtonSignOnClick}>
+                        <FaGithub />
+                        Sign Up With Github
+                    </button>)
+                }
             </div>
             {
                 user ? (
